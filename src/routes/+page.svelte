f89b842599dee112b90579e38a4a95273786e726
Varun Gupta <shadow_flash@Varuns-MacBook-Air.local>
2024-09-13 18:54:18 +0530

    feat: basic initialisation of supabase, svelte and code done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Button from "@/components/ui/button/button.svelte";
+
+</script>
+<h1>Welcome to Stealth Extension!</h1>
+<div>
+    I am a button  from shad cn <Button>Click Me</Button>
+</div>
